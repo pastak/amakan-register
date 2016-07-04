814ecf2f6f1c88f336bcac6ec71fdd9c81f40733
pastak <pasta0915@gmail.com>
2016-07-05 00:54:55 +0900

    1回の実行で勝手にスクレイピングするように変更した

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,85 +1,55 @@
 'use strict';
 
 let nextPageUrl = null
-let notifications = {}
-
-const getAllKindle = (tid) => {
-  chrome.tabs.sendMessage(tid, {action: 'getKindles'}, ({items, nextUrl}) => {
-    items.forEach((item, index) => {
-      window.setTimeout(() => sendToPageUrl(item), index * 100)
-    })
-    nextPageUrl = nextUrl
-    if (nextPageUrl) {
-      chrome.notifications.create({
-        type: 'basic',
-        title: '次のページが存在します',
-        message: '遷移しますか？',
-        iconUrl: 'images/icon-38.png',
-        requireInteraction: true,
-        buttons: [{title: 'No'}, {title: 'Yes'}]
-      })
-    }
-  })
-}
-
-chrome.notifications.onButtonClicked.addListener((nid, buttonIndex) => {
-  chrome.notifications.clear(nid)
-  if (buttonIndex && nextPageUrl) {
-    chrome.tabs.query({active: true}, (tabs) => chrome.tabs.update(tabs[0].id, {url: nextPageUrl}))
-  }
-})
-
-chrome.notifications.onClicked.addListener((nid) => {
-  chrome.tabs.create({url: notifications[nid].url})
-})
+let notificationId = null
 
 const notify = (opt) => new Promise((ok) => {
   chrome.notifications.create(opt, ok)
 })
 
 const sendToPageUrl = ({url, title, imageUrl}) => {
-  notify({type: 'basic', title, iconUrl: imageUrl, message: '登録中...', priority: 1})
-    .then((notifyId) => {
-      notifications[notifyId] = {url: `https://amakan.net/search?query=${url}`}
-      const xhr = new XMLHttpRequest()
-      xhr.open('GET', `https://amakan.net/search?query=${url}`)
-      xhr.onreadystatechange = () => {
-        if (!/amakan\.net\/products\//.test(xhr.responseURL)) return
-        const bookPage = xhr.responseURL
-        console.log(bookPage, url)
-        const html = document.createElement('html')
-        html.innerHTML = xhr.responseText
-        const csrfToken = html.querySelector('meta[name=csrf-token]').content
-        window.fetch(bookPage + '/taste', {
-          method: 'POST',
-          headers: {
-            'X-CSRF-Token': csrfToken
-          },
-          credentials: 'include'
-        })
-          .then((res) => {
-            chrome.notifications.update(notifyId, {
-              message: '登録成功',
-              priority: 0
-            })
-            window.setTimeout(() => chrome.notifications.clear(notifyId), 500)
-          })
-          .catch(() => {
-            chrome.notifications.update(notifyId, {
-              message: '登録不可または何らかのエラーが発生しました',
-              priority: 0
-            })
-            window.setTimeout(() => chrome.notifications.clear(notifyId), 500)
-          })
-      }
-      xhr.send(null)
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', `https://amakan.net/search?query=${url}`)
+  xhr.onreadystatechange = () => {
+    if (!/amakan\.net\/products\//.test(xhr.responseURL)) return
+    const bookPage = xhr.responseURL
+    console.log(bookPage, url)
+    const html = document.createElement('html')
+    html.innerHTML = xhr.responseText
+    const csrfToken = html.querySelector('meta[name=csrf-token]').content
+    window.fetch(bookPage + '/taste', {
+      method: 'POST',
+      headers: {
+        'X-CSRF-Token': csrfToken
+      },
+      credentials: 'include'
     })
+      .then((res) => {
+        chrome.notifications.update(notificationId, {
+          title,
+          iconUrl: imageUrl,
+          message: '登録成功',
+          priority: 0
+        })
+      })
+  }
+  xhr.send(null)
 }
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  sendToPageUrl(request)
+})
 
 chrome.browserAction.onClicked.addListener((tab) => {
   const urlInfo = document.createElement('a')
   urlInfo.href = tab.url
   if (!/amazon\.co\.jp/.test(urlInfo.hostname)) return console.log('🍣')
-  if (/\/order-history/.test(urlInfo.pathname)) return getAllKindle(tab.id)
+  if (/\/order-history/.test(urlInfo.pathname)) {
+    return notify({type: 'basic', title: '開始', iconUrl: 'images/icon-38.png', message: '登録中...', priority: 1, requireInteraction: true})
+      .then((notifyId) => {
+        notificationId = notifyId
+        chrome.tabs.sendMessage(tab.id, {action: 'scrapingAllHistory'})
+      })
+  }
   chrome.tabs.sendMessage(tab.id, {action: 'getBookInfo'}, sendToPageUrl)
 })
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,29 @@
+const fetchPage = (url) => new Promise((ok) => {
+  return window.fetch(url, {credentials: 'include'})
+    .then((res) => res.text())
+    .then(ok)
+})
+
+const scrapingPage = (html) => {
+  const targetDoc = document.createElement('html')
+  targetDoc.innerHTML = html
+  ;[...targetDoc.querySelectorAll('.order > .a-box .a-fixed-right-grid .a-fixed-right-grid-col.a-col-left .a-fixed-left-grid.a-spacing-none .a-fixed-left-grid-inner .a-fixed-left-grid-col.a-col-right')]
+    .forEach((item) => {
+      const url = item.querySelector('.a-link-normal').href
+      const title = item.querySelector('.a-link-normal').textContent.replace(/^[\s\t\n]*(.+)[\s\t\n]*$/, '$1')
+      const imageUrl = item.parentNode.querySelector('img').src
+      chrome.runtime.sendMessage(chrome.runtime.id, {url, title, imageUrl})
+    })
+}
+
+const buildQuery = (baseQuery, index) => {
+  return baseQuery.map((t) => {
+    let [key, value] = t
+    if (key === 'startIndex') value = 10 * index
+    return [key, value].join('=')
+  }).join('&')
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const actions = {
     getBookInfo: () => {
@@ -6,20 +32,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const imageUrl = document.querySelector('#img-canvas > img').src
       sendResponse({url, title, imageUrl})
     },
-    getKindles: () => {
-      const items = [];
-      [...document.querySelectorAll('.order > .a-box .a-fixed-right-grid .a-fixed-right-grid-col.a-col-left .a-fixed-left-grid.a-spacing-none .a-fixed-left-grid-inner .a-fixed-left-grid-col.a-col-right')]
-        .filter((a) => /Kindle/.test(a.textContent))
-        .forEach((item) => {
-          const url = item.querySelector('.a-link-normal').href
-          const title = item.querySelector('.a-link-normal').textContent.replace(/^[\s\t\n]*(.+)[\s\t\n]*$/, '$1')
-          const imageUrl = item.parentNode.querySelector('img').src
-          items.push({url, title, imageUrl})
-        })
-      const nextPageAnchor = document.querySelector('.a-last > a')
-      let nextUrl = null
-      if (nextPageAnchor) nextUrl = nextPageAnchor.href
-      sendResponse({items, nextUrl})
+    scrapingAllHistory: () => {
+      const pathNamePart = '/gp/your-account/order-history/ref=oh_aui_pagination_1_'
+      const queries = document.querySelector('.a-selected > a').search.substr(1).split('&')
+        .map((part) => part.split('='))
+      let t = document.querySelectorAll('.a-normal')
+      const pageCount = +t[t.length - 1].textContent - 1
+      for (let i = 0; i <= pageCount; i++) {
+        const url = pathNamePart + (i + 1) + '?' + buildQuery(queries, i)
+        fetchPage(url)
+          .then((a) => window.setTimeout(() => scrapingPage(a), 1000 * i))
+      }
     }
   }
   if (request.action in actions) {
